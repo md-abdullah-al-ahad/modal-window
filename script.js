bf03d4e790c57504e831d9ccc12d167123593486
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-08-10 05:00:58 +0600

    add modal view functionality to the website

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+const show = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const hide = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', show);
+}
+
+closeModal.addEventListener('click', hide);
+
+overlay.addEventListener('click', hide);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') hide();
+});
